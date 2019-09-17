8ba7b4f6c42cb52862d804029bae1b4a38c26021
ferguson-scott <scott_ferguson@live.ca>
2019-09-17 19:51:38 -0400

    Updating Minecraft world

--- test.cmd
+++ test.cmd
@@ -1,4 +1,6 @@
 @echo off
+echo "Synchronizing Minecraft World"
+git pull
 echo "Starting Minecraft server"
 java -Xmx1024M -Xms1024M -jar server.jar nogui 
 set /p input="To close server enter q, to close without persisting the world enter q -ns: "
