90712c1b9f2512d70509f34f1c46c9cab0644f7f
ferguson-scott <scott_ferguson@live.ca>
2019-09-17 19:48:15 -0400

    Updating Minecraft world

--- test.cmd
+++ test.cmd
@@ -1,4 +1,7 @@
-#!/bin/bash
-dir
-cd "Ultrawide Wallpapers"
-dir
+@echo off
+echo "Starting Minecraft server"
+java -Xmx1024M -Xms1024M -jar server.jar nogui 
+set /p input="To close server enter q, to close without persisting the world enter q -ns: "
+echo "Persisting Minecraft world to GitHub"
+git add .
+git commit -m "Updating Minecraft world"
