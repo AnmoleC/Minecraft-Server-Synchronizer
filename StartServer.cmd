d6d20d1fd80158fe062bb461a45d8c38afb7a986
Scott Ferguson <scott_ferguson@live.ca>
2019-10-28 19:43:08 -0400

    Update StartServer.cmd

--- StartServer.cmd
+++ StartServer.cmd
@@ -3,7 +3,7 @@ echo "Synchronizing Minecraft World"
 git pull
 echo "Starting Minecraft server"
 java -Xmx1024M -Xms1024M -jar server.jar nogui 
-#set /p input="To close server enter q, to close without persisting the world enter q -ns: "
+#set /p input="To close server enter q"
 echo "Persisting Minecraft world to GitHub"
 git add .
 git commit -m "Updating Minecraft world"
