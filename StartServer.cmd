06cfc2b77e2d9f30a8cc295622b2eba7b8bc771b
Anmole Chaudhry <anmole_chaudry@hotmail.com>
2020-04-17 12:09:07 -0400

    Removed Input q to quit. Added pause at end

--- StartServer.cmd
+++ StartServer.cmd
@@ -3,8 +3,8 @@ echo "Synchronizing Minecraft World"
 git pull
 echo "Starting Minecraft server"
 java -Xms4096M -Xmx8192M -jar server.jar nogui  
-#set /p input="To close server enter q"
 echo "Persisting Minecraft world to GitHub"
 git add .
 git commit -m "Updating Minecraft world"
 git push
+pause
